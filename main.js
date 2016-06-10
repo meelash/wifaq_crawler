9a877dde0a92aafef554e829190fc8d985793a88
Saleem Abdul Hamid <meelash@gmail.com>
2016-06-10 11:29:38 -0400

    Handle errors and resubmit requests on error

--- main.js
+++ main.js
@@ -6,39 +6,46 @@ var urlStart = "http://www.wifaqulmadaris.org/_reports/gazett-madaris.php?gender
 var urlEnd = "&Submit=";
 
 var ilhaqNumber = 00000;
-var totalCount = 9000;
+var totalCount = 1000;
 var list = []
 var returnCount = 0;
 
 console.log('starting');
 
+var makeRequest = (ilhaqNumber) => {
+  var req = request({ uri: urlStart + ilhaqNumber + urlEnd, timeout: 120000 }, (error, response, body) => {
+    if (error) {
+      console.warn('error at ilhaq number ' + ilhaqNumber + ': ' + error);
+      makeRequest(ilhaqNumber);
+      return;
+    }
+    if (!error && response.statusCode == 200) {
+      var startLoc = body.indexOf('z-index:25;top:55px;left:663px;width:341px;height:38px;text-align:right;') + 119;
+      var subStr = body.substr(startLoc, 1000);
+      var endLoc = subStr.indexOf(' </span></div>');
+      var result = subStr.slice(0, endLoc);
+      // console.log(result);
+      if (!!result) {
+        list.push({ilhaqNumber: ilhaqNumber, school: result});
+      }
+    }
+    returnCount++;
+    console.log(returnCount + ' for ' + ilhaqNumber);
+    if (returnCount >= totalCount){
+      onEnd();
+      
+    }
+  })
+  // console.log(req);
+}
+
 while (ilhaqNumber < totalCount) {
-  ((ilhaqNumber) => {
-    var req = request(urlStart + ilhaqNumber + urlEnd, (error, response, body) => {
-      if (!error && response.statusCode == 200) {
-        var startLoc = body.indexOf('z-index:25;top:55px;left:663px;width:341px;height:38px;text-align:right;') + 119;
-        var subStr = body.substr(startLoc, 1000);
-        var endLoc = subStr.indexOf(' </span></div>');
-        var result = subStr.slice(0, endLoc);
-        // console.log(result);
-        if (!!result) {
-          list.push({ilhaqNumber: ilhaqNumber, school: result});
-        }
-      }
-      returnCount++;
-      console.log(returnCount + ' for ' + ilhaqNumber);
-      if (returnCount >= totalCount){
-        onEnd();
-        
-      }
-    })
-    // console.log(req);
-  })(ilhaqNumber);
+  makeRequest(ilhaqNumber);
   
   ilhaqNumber++;
 }
 
-onEnd = () => {
+var onEnd = () => {
   json2csv({ data: list, fields: ['ilhaqNumber', 'school'] }, function(err, csv) {
     if (err) console.log(err);
     fs.writeFile('ilhaqNumbers.csv', csv, function(err) {
